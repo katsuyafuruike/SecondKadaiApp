33b8fc8668657656ff8f6d45971b1b4922dc68d0
古池勝也 <furuike0816@gmail.com>
2019-01-14 20:14:57 +0900

    アウトレットの間違い直した。（次はレイアウト）

--- SecondKadaiApp/ResultViewController.swift
+++ SecondKadaiApp/ResultViewController.swift
@@ -1,34 +1,16 @@
-//
-//  ResultViewController.swift
-//  SecondKadaiApp
-//
-//  Created by 古池勝也 on 2019/01/14.
-//  Copyright © 2019 katsuya.furuike. All rights reserved.
-//
-
 import UIKit
 
 class ResultViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
     
-    
+    @IBOutlet weak var label2: UILabel!
+    var x:String? = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let result = x
+        label2.text = "こんにちは、\(result)さん"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -1,26 +1,23 @@
-//
-//  ViewController.swift
-//  SecondKadaiApp
-//
-//  Created by 古池勝也 on 2019/01/14.
-//  Copyright © 2019 katsuya.furuike. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
-    @IBOutlet weak var namae: UILabel!
+    @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var textfield: UITextField!
     
+    var input = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.x = textfield.text!
+    }
+    
     @IBAction func unwind(_ segue: UIStoryboardSegue){
     }
-
+    
 }
 
